7aae0d69b83e84725c36f8005e4abbc6770f9376
kayroc <kayroc@126.com>
2023-03-26 17:05:33 +0800

    添加前端目录结构

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,3 +1,10 @@
 module.exports = [
-  { text: '导航', link: '/' }
+  { text: '导航', link: '/' },
+  {
+    text: '前端',
+    link: '/web/', // 目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      { text: 'HTML', link: '/pages/7e611f/' } // 注意link结尾有斜杠和没有斜杠的区别
+    ]
+  }
 ]
