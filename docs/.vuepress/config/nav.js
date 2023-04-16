537eb09281067d49bc1b616acf92b4b2573ff3eb
kayroc <kayroc@126.com>
2023-04-17 00:39:09 +0800

    增加CSS导航

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,7 +4,8 @@ module.exports = [
     text: '前端',
     link: '/web/', // 目录页，vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
-      { text: 'HTML', link: '/pages/7e611f/' } // 注意link结尾有斜杠和没有斜杠的区别
+      { text: 'HTML', link: '/pages/7e611f/' }, // 注意link结尾有斜杠和没有斜杠的区别
+      { text: 'CSS', link: '/pages/5d7ab2/' }
     ]
   }
 ]
